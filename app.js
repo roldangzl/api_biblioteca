2b4f1daea0a6b5fd18612d692c75c33306c3f8a7
roldangzl <roldan.gzl@hotmail.com>
2023-12-18 19:44:12 -0300

    first commit

--- app.js
+++ app.js
@@ -1,4 +1,18 @@
 const express = require('express');
+
+// Importamos la libreria express-oauth2-jwt-bearer
+const { auth } = require("express-oauth2-jwt-bearer");
+
+// Importamos el Middleware Error Handler
+const errorHandler = require('./middleware/errorHandler');
+
+// Configuracion Middleware con el Servidor de Autorización
+const autenticacion = auth({
+    audience: 'https://api.actividadclase6.com/api/libros',
+    issuerBaseURL: 'https://dev-run172m6qa6lt7li.us.auth0.com/',
+    tokenSigningAlg: 'RS256'
+  });
+
 const app = express();
 app.use(express.json());
 
@@ -9,6 +23,8 @@ const librosRouter = require('./routes/libros');
 const errorHandler = require('./middleware/errorHandler');
 
 app.use('/libros', librosRouter);
+// Configuramos el middleware de autenticación
+app.use('/libros', autenticacion, librosRouter);
 
 app.use(errorHandler);
 
